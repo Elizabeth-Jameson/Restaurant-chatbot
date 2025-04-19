376d344b52900fb5c2a3d8cb75542ba94067513a
Elizabeth-Jameson <elizabethjameson03@gmail.com>
2025-04-19 22:11:03 +0100

    the initial commit of the project.

---
+++ utils/menuItems.js
@@ -0,0 +1,18 @@
+const menuItems = [
+    { id: 1, name:"Fried Rice with Beef", price: 7000 },
+    { id: 2, name: "Jollof Rice with Chicken", price: 6500 },
+    { id: 3, name: "Eba and Edikanikon", price: 6000 },
+    { id: 4, name: "Amala and Ewedu", price: 3700 },
+    { id: 5, name: "Beans and Plantain", price: 2400 },
+    { id: 6, name: "Ekpang nkokwo", price: 8500 },
+    { id: 7, name: "Pounded Yam and Egusi Soup", price: 4000 },
+    { id: 8, name: "Ofada Rice and Stew", price: 3500 },
+    { id: 9, name: "Chicken and Chips", price: 9500 },
+    {id: 10, name: "white Rice with Stew", price: 5000 },
+    {id: 11, name: "Afang soup and fufu", price: 5000},
+    {id: 13, name: "A glass of fresh juice", price: 3500}
+  ]
+   
+  
+  module.exports = { menuItems };
+  
